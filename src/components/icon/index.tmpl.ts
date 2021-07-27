12d01e863287ce5a94511fbbe955f88de1d7b8b6
Valentine <valentine.kirsanova@gmail.com>
2021-07-27 20:33:45 +0300

    added styles for icon

--- src/components/icon/index.tmpl.ts
+++ src/components/icon/index.tmpl.ts
@@ -1,6 +1,9 @@
 const tmpl: string = `
-    <span class="material-icons">
-        {{ id }}
+    <span
+        class="material-icons"
+        style="color: {{ color }}; font-size: {{ size }}px;"
+        >
+         {{ id }}
     </span>
 `;
 
