cc7fc51c4d3b3c56d16c2e3f01f96ad5b3d536af
Valentine <valentine.kirsanova@gmail.com>
2021-07-30 18:00:26 +0300

    draft for validation

--- src/layouts/unauth/UnauthPageLayout.ts
+++ src/layouts/unauth/UnauthPageLayout.ts
@@ -2,8 +2,11 @@ import tmpl from './index.tpml';
 import UnauthPageType from './unauthPageType';
 import CreateLayout from '../CreateLayout';
 import collectFormFieldsMixin from '../../mixins/collectFormFields';
+import validateFieldMixin from '../../mixins/validateFieldMixin';
 
-Object.assign(CreateLayout.prototype, collectFormFieldsMixin);
+Object.assign(CreateLayout.prototype,
+    collectFormFieldsMixin,
+    validateFieldMixin);
 
 export default class UnauthPageLayout extends CreateLayout {
     constructor(ctx: UnauthPageType) {
@@ -16,11 +19,25 @@ export default class UnauthPageLayout extends CreateLayout {
     initEventListeners() {
         super.initEventListeners();
         this.collectFields();
+        this.initInputsValidation();
     }
 
     collectFields() {
         const submitButton = this.element
             .querySelector('button[type="submit"]');
         submitButton.addEventListener('click', this.collectFormFields);
+    }
+
+    initInputsValidation(): void {
+        const inputs: HTMLElement[] | null = [...this.element.querySelectorAll('input')];
+        const hasInputs = Boolean(inputs) === true;
+
+        if (!hasInputs) {
+            return;
+        }
+
+        inputs.forEach((input: HTMLElement) => {
+            input.addEventListener('blur', this.validateField);
+        });
     }
 }
