ddea6afba40983d44edaeb19f435866c69f43cb2
Valentine <valentine.kirsanova@gmail.com>
2021-08-01 18:26:15 +0300

    refactored change password page

--- src/pages/changePassword/index.ts
+++ src/pages/changePassword/index.ts
@@ -1,35 +1,49 @@
-import tmpl from '../../layouts/profile/index.tmpl';
-import renderTemplate from '../../utils/renderTemplate';
+import FormInput from '../../components/formInput';
 
-const renderInputs = {
-    oldPassword: {
+const inputs = [
+    {
         label: 'Старый пароль',
         value: '',
         name: 'oldPassword',
         type: 'password',
     },
-    newPassword: {
+    {
         label: 'Новый пароль',
         value: '',
         name: 'newPassword',
         type: 'password',
     },
-    submitPassword: {
+    {
         label: 'Повторите новый пароль',
         value: '',
         name: 'submitPassword',
         type: 'password',
     },
-};
+];
+
+import ProfilePageLayout from '../../layouts/profile/ProfilePageLayout';
+import renderPage from '../../utils/renderPage';
+import Button from '../../components/button';
+import Form from '../../components/form';
+
+const childrenInputs = inputs
+    .map(input => new FormInput({...input, class: 'profile-input'}));
 
 const controls = [
-    {
+    new Button({
         label: 'Сохранить',
-    },
+    }),
 ];
-const renderData = {
-    inputs: renderInputs,
-    controls,
-};
 
-renderTemplate(tmpl, renderData, '#root');
+const ctx = {
+    children: {
+        form: new Form({
+            children: {
+                inputs: childrenInputs,
+            },
+        }),
+        controls,
+    },
+};
+const showProfilePage = new ProfilePageLayout(ctx);
+renderPage(showProfilePage);
