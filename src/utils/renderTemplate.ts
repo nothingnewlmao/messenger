1799d0f24f4e0786a57e474a175b1788fe774479
Valentine <valentine.kirsanova@gmail.com>
2021-08-02 19:29:08 +0300

    render template ts fix

--- src/utils/renderTemplate.ts
+++ src/utils/renderTemplate.ts
@@ -1,6 +1,6 @@
 import * as Handlebars from 'handlebars';
 
-export default function renderTemplate(template, ctx, selector) {
+export default function renderTemplate(template: string, ctx: Object, selector: string) {
     const _template = Handlebars.compile(template);
     // eslint-disable-next-line no-undef
     document.querySelector(selector).innerHTML = _template(ctx);
