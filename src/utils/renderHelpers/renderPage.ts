a3af10a56afb963ecb5bcb1b6ec0da2b96d58f22
Valentine <valentine.kirsanova@gmail.com>
2021-08-07 21:59:26 +0300

    Object Literal for render helpers

--- src/utils/renderHelpers/renderPage.ts
+++ src/utils/renderHelpers/renderPage.ts
@@ -1,6 +1,6 @@
-import ObjectType from '../../types/ObjectType';
+import ObjectLiteral from '../../types/ObjectLiteral';
 
-export default function renderPage(page: ObjectType): void {
+export default function renderPage(page: ObjectLiteral): void {
     const target = document.querySelector('#root');
     target.append(page.getContent());
 }
--- src/utils/renderHelpers/renderTemplate.ts
+++ src/utils/renderHelpers/renderTemplate.ts
@@ -1,7 +1,7 @@
 import * as Handlebars from 'handlebars';
-import ObjectType from '../../types/ObjectType';
+import ObjectLiteral from '../../types/ObjectLiteral';
 
-export default function renderTemplate(template: string, ctx: ObjectType, selector: string) {
+export default function renderTemplate(template: string, ctx: ObjectLiteral, selector: string) {
     const _template = Handlebars.compile(template);
     // eslint-disable-next-line no-undef
     document.querySelector(selector).innerHTML = _template(ctx);
