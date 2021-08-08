57eee1ccba979746685165811166baae564991de
Valentine <valentine.kirsanova@gmail.com>
2021-08-08 12:54:08 +0300

    deleted search

--- src/components/search/index.ts
+++
@@ -1,8 +0,0 @@
-import * as Handlebars from 'handlebars';
-import tmpl from './index.tmpl';
-import './index.scss';
-
-export default function regSearch() {
-    const search = Handlebars.compile(tmpl);
-    Handlebars.registerPartial('search', search);
-}
