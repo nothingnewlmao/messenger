4aea5db0ff015595cb45101aa5ba16a7ff920e0e
oduvankenobi <valentine.kirsanova@gmail.com>
2021-08-09 18:32:13 +0300

    httpTransports to ts

--- src/utils/HTTPTransport.ts
+++ src/utils/HTTPTransport.ts
@@ -1,77 +1,73 @@
-const METHODS = {
-    GET: 'GET',
-    POST: 'POST',
-    PUT: 'PUT',
-    DELETE: 'DELETE',
-};
-
-function queryStringify(data) {
-    if (typeof data !== 'object') {
-        throw new Error('Data must be object');
-    }
-
-    const keys = Object.keys(data);
-    return keys.reduce((result, key, index) => {
-        return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`;
-    }, '?');
+enum METHODS {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE'
 }
 
-export default class HTTPTransport {
-    get = (url, options = {}) => {
+type Options = {
+    method: METHODS,
+    timeout?: number,
+    headers?: Record<string, string>,
+    data?: any
+};
 
-        return this.request(url, {...options, method: METHODS.GET}, options.timeout);
-    };
+function queryStringify(data: Record<string, any>) {
+    const noData = !data && Boolean(Object.keys(data).length);
+    if (noData) {
+        return '';
+    }
 
-    post = (url, options = {}) => {
-        return this.request(url, {...options, method: METHODS.POST}, options.timeout);
-    };
+    const queryString = Object.entries(data)
+        .reduce((queryString: string, [key, value]) => `${queryString}${key}=${value}&`, '?');
+    queryString.slice(-1);
+    return queryString;
+}
 
-    put = (url, options = {}) => {
-        return this.request(url, {...options, method: METHODS.PUT}, options.timeout);
-    };
+class HTTPTransport {
+    get = (url: string, options: Options = {method: METHODS.GET, timeout: 3000}) => {
+        const {data} = options;
+        const queryString = `${url}${queryStringify(data)}`;
+        return this.request(queryString, options);
+    }
 
-    delete = (url, options = {}) => {
-        return this.request(url, {...options, method: METHODS.DELETE}, options.timeout);
-    };
+    post = (url: string,
+        options = {method: METHODS.POST, data: {}}): Promise<XMLHttpRequest> => this
+        .request(url, options)
 
-    request = (url, options = {}, timeout = 5000) => {
+    put = (url: string,
+        options = {method: METHODS.PUT}): Promise<XMLHttpRequest> => this
+        .request(url, options)
+
+    delete = (url: string, options = {method: METHODS.DELETE}) => this.request(url, options)
+
+    request = (url: string,
+        options: Options = {method: METHODS.GET, timeout: 5000}): Promise<XMLHttpRequest> => {
         const {
-            headers = {},
             method,
-            data,
+            data = {},
+            timeout,
+            headers = {},
         } = options;
 
-        return new Promise(function(resolve, reject) {
-            if (!method) {
-                reject('No method');
-                return;
-            }
+        return new Promise((resolve, reject) => {
+            const xhr: XMLHttpRequest = new XMLHttpRequest();
+            xhr.open(method, url);
 
-            const xhr = new XMLHttpRequest();
-            const isGet = method === METHODS.GET;
-
-            xhr.open(
-                method,
-                isGet && Boolean(data)
-                    ? `${url}${queryStringify(data)}`
-                    : url,
-            );
-
-            Object.keys(headers).forEach(key => {
-                xhr.setRequestHeader(key, headers[key]);
-            });
-
-            xhr.onload = function () {
-                resolve(xhr);
-            };
-
-            xhr.onabort = reject;
             xhr.onerror = reject;
+            xhr.onabort = reject;
 
             xhr.timeout = timeout;
             xhr.ontimeout = reject;
 
-            if (isGet || !data) {
+            Object.entries(headers)
+                .forEach(([key, value]: [string, string]) => {
+                    xhr.setRequestHeader(key, value);
+                });
+
+            xhr.onload = () => resolve(xhr);
+
+            if (method === METHODS.GET || !data) {
                 xhr.send();
             } else {
                 xhr.send(data);
