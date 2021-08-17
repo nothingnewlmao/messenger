f13715256446f63b410526570a5e9d27c48cf422
Valentine <valentine.kirsanova@gmail.com>
2021-08-17 21:20:55 +0300

    added basic api class

---
+++ src/api/BasicAPI.ts
@@ -0,0 +1,19 @@
+class BasicAPI {
+    create() {
+        throw new Error('Not implemented.');
+    }
+
+    request() {
+        throw new Error('Not implemented.');
+    }
+
+    edit() {
+        throw new Error('Not implemented.');
+    }
+
+    delete() {
+        throw new Error('Not implemented.');
+    }
+}
+
+export default BasicAPI;
