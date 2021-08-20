ac27ac70d62f199f9ddf8bf6dd93969d2ea994db
Valentine <valentine.kirsanova@gmail.com>
2021-08-20 18:21:14 +0300

    set values from BE on settings main page

--- src/pages/settings/show/SettingsShowPage.ts
+++ src/pages/settings/show/SettingsShowPage.ts
@@ -3,6 +3,10 @@ import tmpl from '../../../layouts/settings/index.tmpl';
 import ObjectLiteral from '../../../types/ObjectLiteral';
 import UserController from '../../../controllers/UserController';
 
+import FormInput from '../../../components/formInput';
+import deepClone from '../../../utils/functions/deepClone';
+import setProperty from '../../../utils/functions/setProperty';
+
 export default class SettingsShowPage extends Block {
     constructor(ctx: ObjectLiteral) {
         super('div', {
@@ -19,6 +23,19 @@ export default class SettingsShowPage extends Block {
     async componentDidMount() {
         super.componentDidMount();
         const userController = new UserController();
-        const info = await userController.getUserInfo();
+        const info = await userController
+            .getUserInfo()
+            .then(response => JSON.parse(response));
+        this.mapInputsValues(info);
+    }
+
+    mapInputsValues(valuesObject: ObjectLiteral) {
+        const {inputs} = this.props.ctx.children.form.props.ctx.children;
+        inputs.forEach((input: FormInput) => {
+            const propsClone = deepClone(input.props);
+            const {name: inputName} = input.props.ctx;
+            setProperty(propsClone, 'ctx.value', valuesObject[inputName]);
+            input.setProps(propsClone);
+        });
     }
 }
