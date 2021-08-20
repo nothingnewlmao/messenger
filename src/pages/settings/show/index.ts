cd3513cba491bc22034165995d2e3ba89fde2edf
Valentine <valentine.kirsanova@gmail.com>
2021-08-20 18:20:55 +0300

    refactored main settings page

--- src/pages/settings/show/index.ts
+++ src/pages/settings/show/index.ts
@@ -7,30 +7,12 @@ import UserController from '../../../controllers/UserController';
 
 const userController = new UserController();
 
-const controls = [
-    new Button({
-        label: 'Изменить данные',
-        className: '_flat',
-    }),
-    new Button({
-        label: 'Изменить пароль',
-        className: '_flat',
-    }),
-    new Button({
-        label: 'Выйти',
-        className: '_flat _negative',
-    }, {
-        click: () => {
-            userController.logout();
-        },
-    }),
-];
-
 const childrenInputs = inputs
     .map(input => new FormInput({
         ...input,
         className: 'profile-input',
     }));
+
 const ctx = {
     children: {
         form: new Form({
@@ -38,7 +20,24 @@ const ctx = {
                 inputs: childrenInputs,
             },
         }),
-        controls,
+        controls: [
+            new Button({
+                label: 'Изменить данные',
+                className: '_flat',
+            }),
+            new Button({
+                label: 'Изменить пароль',
+                className: '_flat',
+            }),
+            new Button({
+                label: 'Выйти',
+                className: '_flat _negative',
+            }, {
+                click: () => {
+                    userController.logout();
+                },
+            }),
+        ],
     },
 };
 
